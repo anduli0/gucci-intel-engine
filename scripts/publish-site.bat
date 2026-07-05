ddb213b374c21dbcb94d20b59be1546c9c278ff8
anduli0 <minkyu494@gmail.com>
2026-07-05 15:10:39 +0900

    publish-site.bat: identity for rebase

--- scripts/publish-site.bat
+++ scripts/publish-site.bat
@@ -4,7 +4,7 @@ rem GitHub Pages serves gucci-intel-engine:/docs.
 cd /d C:\Users\andul\gucci-intel
 set "EXPORT_OUT=C:\Users\andul\gucci-intel\docs"
 python scripts\export_static.py >> logs\publish-site.log 2>&1
-git pull --rebase origin main >> logs\publish-site.log 2>&1
+git -c user.name="anduli0" -c user.email="minkyu494@gmail.com" pull --rebase origin main >> logs\publish-site.log 2>&1
 git add docs data >> logs\publish-site.log 2>&1
 git -c user.name="anduli0" -c user.email="minkyu494@gmail.com" commit -m "local publish %date% %time%" >> logs\publish-site.log 2>&1
 git push >> logs\publish-site.log 2>&1
